2cd108656045fb93774a076d8acff14e97348975
Julian Bastow <julian.bastow@storyxag.com>
2022-12-03 02:03:29 +0100

    add post skeleton

---
+++ pages/posts/page.tsx
@@ -0,0 +1,9 @@
+import Layout from "../../components/layout";
+
+export default function Page() {
+  return (
+    <div className="container mx-auto w-1/2 py-16 md:px-2">
+      <div className="flex justify-center"></div>
+    </div>
+  );
+}
