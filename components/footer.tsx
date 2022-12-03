e88667c184a8d50703b95181d1dece0d12956f2f
Julian Bastow <julian.bastow@storyxag.com>
2022-12-03 01:40:39 +0100

    latest posts display, footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,3 +1,27 @@
+import { FaInstagram, FaMastodon } from "react-icons/fa";
+import Link from "next/link";
+
 export default function footer() {
-  return <div className="bg-gray-500">footer</div>;
+  return (
+    <footer className="bg-gray-50">
+      <div className="container mx-auto justify-center py-12">
+        <div className="py-5">
+          <div className="flex justify-center gap-6">
+            <Link href={"/"}>
+              <FaInstagram color="c0c0c0" />
+            </Link>
+            <Link href={"/"}>
+              <FaMastodon color="c0c0c0" />
+            </Link>
+          </div>
+        </div>
+        <p className="py-5 text-center text-gray-400">
+          Copyright ©2022 All rights reserved
+        </p>
+        <p className=" text-center text-gray-400">
+          Contact | Privacy | Legal notice
+        </p>
+      </div>
+    </footer>
+  );
 }
--- components/latest.tsx
+++ components/latest.tsx
@@ -6,11 +6,53 @@ export default function latest() {
     <div className="container mx-auto py-16 md:px-20">
       <>
         <h1 className="pb-12 text-center text-3xl font-bold">Latest Posts</h1>
-        <div className="grid gap-14 md:grid-cols-2 lg:grid-cols-3"></div>
-        {Post()}
+        <div className="grid gap-14 md:grid-cols-3 lg:grid-cols-4">
+          {Post()}
+          {Post()}
+          {Post()}
+          {Post()}
+          {Post()}
+          {Post()}
+          {Post()}
+          {Post()}
+        </div>
       </>
     </div>
   );
 }
 
-function Post() {}
+function Post() {
+  return (
+    <div className="item">
+      <div className="image">
+        {" "}
+        <Link href={"/"}>
+          <Image
+            src={"/images/showcase1.jpg"}
+            width={200}
+            height={200}
+            alt="image"
+            className="rounded-full "
+          />
+        </Link>
+      </div>
+      <div className="info flex flex-col justify-center py-4">
+        <div className="cat mb-2">
+          <Link href={"/"}>Categories - Date</Link>
+        </div>
+        <div className="title mb-5">
+          <Link href={"/"} className="text-2xl md:text-3xl">
+            Lorem ipsum
+          </Link>
+        </div>
+        <div className="description">
+          <Link href={"/"} className="">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua.
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
