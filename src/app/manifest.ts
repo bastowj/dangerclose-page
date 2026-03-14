4095c3550f692720a587144d03dee330ef470ba6
Julian Bastow <julian.bastow@storyxag.com>
2026-03-15 00:47:07 +0100

    replace site.webmanifest with manifest.ts

---
+++ src/app/manifest.ts
@@ -0,0 +1,16 @@
+import type { MetadataRoute } from "next";
+import { SITE_CONFIG } from "@/constants/config";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: SITE_CONFIG.defaultTitle,
+    short_name: "Danger Close!",
+    icons: [
+      { src: "/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
+      { src: "/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
+    ],
+    theme_color: "#ffffff",
+    background_color: "#ffffff",
+    display: "standalone",
+  };
+}
